6679a939b7673d432b07a95e0ef10d4490606809
Markus Siemens <siemens1993@gmail.com>
2015-03-12 17:26:13 +0100

    Add an abort method for errors without source

--- src/driver/error.rs
+++ src/driver/error.rs
@@ -3,28 +3,23 @@ use std::old_io;
 use ansi_term::Colour::{Red, Yellow};
 use driver::codemap::Loc;
 
-#[macro_export]
-macro_rules! fatal(
-    ($msg:expr, $($args:expr),* ; $stmt:expr) => {
-        {
-            use assembler::util::fatal;
-            fatal(format!($msg, $($args),*), &$stmt.location)
-        }
-    };
-
-    ($msg:expr ; $stmt:expr) => {
-        {
-            use std::borrow::ToOwned;
-            ::assembler::util::fatal($msg.to_owned(), &$stmt.location)
-        }
-    };
-);
-
 
 fn is_tty() -> bool {
     old_io::stdio::stderr_raw().isatty()
 }
 
+pub fn abort(msg: String) -> ! {
+    let mut stderr = io::stderr();
+
+    if is_tty() {
+        writeln!(&mut stderr, "{}: {}", Red.paint("Error"), msg).ok();
+    } else {
+        writeln!(&mut stderr, "Error: {}", msg).ok();
+    }
+
+    old_io::stdio::set_stderr(Box::new(old_io::util::NullWriter));
+    panic!();
+}
 
 pub fn fatal(msg: String, source: Loc) -> ! {
     let mut stderr = io::stderr();
@@ -38,14 +33,6 @@ pub fn fatal(msg: String, source: Loc) -> ! {
     old_io::stdio::set_stderr(Box::new(old_io::util::NullWriter));
     panic!();
 }
-
-
-#[macro_export]
-macro_rules! warn(
-    ($msg:expr, $($args:expr),* ; $stmt:expr ) => {
-        ::assembler::util::warn(format!($msg, $($args),*), &$stmt.location)
-    }
-);
 
 pub fn warn(msg: String, source: Loc) {
     let mut stderr = io::stderr();
